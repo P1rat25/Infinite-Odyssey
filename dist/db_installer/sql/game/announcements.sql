930ffe16df2f06f0e7cdd40c0853cdd67324857f
Prototype-Prodject <prototype.prodjekt@yandex.ru>
2015-10-29 05:12:12 +0200

    Add

--- dist/db_installer/sql/game/announcements.sql
+++ dist/db_installer/sql/game/announcements.sql
@@ -11,4 +11,4 @@ CREATE TABLE IF NOT EXISTS `announcements` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO announcements (`type`, `author`, `content`) VALUES 
-(0, 'Mobius', 'Server powered by http://prototype-project.ru/ project.');
+(0, 'Prototype', 'Server powered by http://prototype-project.ru/ project.');
